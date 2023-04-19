528c0f44edfd08f9603e0562d6def0696d0d2973
KevinFrutos <vinke.14297@gmail.com>
2023-04-19 17:50:29 +0200

    cmsupdate: Added endpoint for updating cms on domains

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/cmsrequest/{number}', [CMSController::class, 'getDomains']);
+Route::post('/cmsupdate', [CMSController::class, 'setCMSName']);
