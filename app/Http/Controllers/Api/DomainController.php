8b2462d2daabb1c9e9fc1974295c100d9f50889b
Samuel Araujo <samuel.araujo@studiogenesis.es>
2023-04-24 19:49:49 +0200

    Add DomainController

---
+++ app/Http/Controllers/Api/DomainController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Domain;
+use Illuminate\Http\Request;
+
+class DomainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $domains = Domains::paginate(15);
+        
+        return view('back.categories.listado', compact('domains'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // return redirect()
+        //     ->action('Back\CategoriesController@index')
+        //     ->with(['success' => $messages]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Domain $domain)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Domain $domain)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Domain $domain)
+    {
+        //
+    }
+
+    public function getDomains(Request $request)
+    {
+        $domains = Domain::all();
+
+        return response()->json(['status' => 1, 'domains' => $domains]);
+
+    }
+
+}
