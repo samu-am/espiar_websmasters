624afc6e59045efd9b3148c311ec723b786941bf
danimacs <dani-mac@hotmail.es>
2023-04-24 20:26:19 +0200

    2 endpoints per guardar el raw del Whois

---
+++ app/Http/Controllers/WhoisController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Domain;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class WhoisController extends Controller
+{
+    public function getWhois(): Response
+    {
+        $domain = Domain::select("domain")->whereNull('whois_raw')->inRandomOrder()->first();
+        return new Response(json_encode($domain), 200, ['content-type' => 'application/json']);
+    }
+
+    public function setWhois(Request $request): Response
+    {
+        $domain = $request->get('domain');
+        $whois = $request->get('whois_raw');
+
+        if ($domain !== null && $whois !== null) {
+            return new Response(json_encode(['status' => "Algún dato no es correcto"]), 400, ['content-type' => 'application/json']);
+        }
+
+        Domain::where('domain', $domain)->update(['whois_raw' => $whois]);
+        return new Response(json_encode(['status' => 'Datos añadidos correctamente']), 200, ['content-type' => 'application/json']);
+    }
+}
